a15c0c67a1c9fa960583403eff435ee6e9488168
hanjuseung <rhdqn0501@naver.com>
2025-04-14 11:35:39 +0900

    자바 시작, gitignore 추가

---
+++ src/study/start/Hello.java
@@ -0,0 +1,11 @@
+package study.start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!!");
+		// 실행 (Run) : 
+	}
+
+}
