9c8e5bceeef7594636ad7fc0ab37747b942ac087
hanjuseung <rhdqn0501@naver.com>
2025-04-28 16:59:22 +0900

    클래스 시작, 필드 && 생성자

--- src/study/refer/Reference07.java
+++ src/study/refer/Reference07.java
@@ -1,10 +1,37 @@
 package study.refer;
 
+import java.util.Scanner;
+
 public class Reference07 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String day1 = "MONDAY";
+		String day2 = "FRIDAY";
+		
+		String today = "FRIDAY"; //사용자가 입력한 값
+		
+		if(today.equals(day1)) {
+			System.out.println("출근");
+		}
+		
+		if(today.equals(day2)) {
+			System.out.println("퇴근");
+		}
+		
+		//붕어빵 토핑 : ppat , sucream, kimchi, pizza
+		String topping = "ppat";
+		
+		Week day3 = Week.MON;
+		Week day4 = Week.FRI;
+		
+		if(day4 == Week.FRI) {
+			System.out.println("금요일이구나~");
+		}
+		
+		Topping tp1 = Topping.SUCREAM;
+		
 	}
 
 }
