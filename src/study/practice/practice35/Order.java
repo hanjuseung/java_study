0f0ec2eb5cf0907d1e142b07e69a5cf5f5d384f0
hanjuseung <rhdqn0501@naver.com>
2025-05-07 10:47:00 +0900

    연습35 클래스

---
+++ src/study/practice/practice35/Order.java
@@ -0,0 +1,27 @@
+package study.practice.practice35;
+
+public class Order {
+	
+	// 필드(인스턴스 필드)
+	int orderNum;
+	Menu[] menus;
+	// 생성자
+	public Order(int i, Menu[] arr) {
+	orderNum = i;
+	menus = arr;
+	}
+	// 메소드(인스턴스 메소드)
+	public int totalPrice() {
+	/* 1. 모든 주문 메뉴의 총합을 반환하세요. */
+		int total=0;
+		for(int i=0;i<menus.length;i++) {
+			total += menus[i].price;
+		}
+		
+//		for(Menu i : menus) {
+//			total += i.price;
+//		}
+	return total;
+	}
+
+}
