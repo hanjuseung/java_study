ea52f6baff8dfb154ea14d23d3273698c96082f8
hanjuseung <rhdqn0501@naver.com>
2025-05-09 14:30:44 +0900

    클래스 상속 강제타입변환

--- src/study/inherit/inherit06/Bus.java
+++ src/study/inherit/inherit06/Bus.java
@@ -1,7 +1,10 @@
 package study.inherit.inherit06;
 
 public class Bus extends Vehicle{
-public void run() {
+	public void run() {
 		System.out.println("Bus run");
+	}
+	public void start() {
+		System.out.println("운행 시작!");
 	}
 }
