4dcaa0c4e176ff2b4c1a14d52c47904bd69ef31a
hanjuseung <rhdqn0501@naver.com>
2025-05-12 16:29:03 +0900

    추상클래스 (+추상메소드)

---
+++ src/study/inherit/abst01/Coke.java
@@ -0,0 +1,12 @@
+package study.inherit.abst01;
+//콜라
+public class Coke extends Beverage {
+
+	public void checkSafety() {
+		System.out.println("탄산 당 정상법위 체크");
+	}
+
+	public void open() {
+		System.out.println("뚜껑 딸깍");
+	}
+}
